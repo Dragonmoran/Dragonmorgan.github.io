2324b92896a2385a4146ca831374f5c73fe702ae
Carl Bach <carloscar3539@gmail.com>
2023-11-20 14:36:18 +0100

    i

---
+++ test.js
@@ -0,0 +1,120 @@
+function myFunction1()
+{
+    var dots = document.getElementById("dots");
+    var hidden = document.getElementById("hidden");
+    var btn = document.getElementById("mybtn");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+}
+function myFunction2()
+{
+    var dots = document.getElementById("dots2");
+    var hidden = document.getElementById("hidden2");
+    var btn = document.getElementById("mybtn2");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction3()
+{
+    var dots = document.getElementById("dots3");
+    var hidden = document.getElementById("hidden3");
+    var btn = document.getElementById("mybtn3");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction4()
+{
+    var dots = document.getElementById("dots4");
+    var hidden = document.getElementById("hidden4");
+    var btn = document.getElementById("mybtn4");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction5()
+{
+    var dots = document.getElementById("dots5");
+    var hidden = document.getElementById("hidden5");
+    var btn = document.getElementById("mybtn5");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction6()
+{
+    var dots = document.getElementById("dots6");
+    var hidden = document.getElementById("hidden6");
+    var btn = document.getElementById("mybtn6");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction7()
+{
+    var dots = document.getElementById("dots7");
+    var hidden = document.getElementById("hidden7");
+    var btn = document.getElementById("mybtn7");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
+function myFunction8()
+{
+    var dots = document.getElementById("dots8");
+    var hidden = document.getElementById("hidden8");
+    var btn = document.getElementById("mybtn8");
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Læs mere"; 
+        hidden.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Læs mindre";
+        hidden.style.display = "inline";
+      }
+} 
